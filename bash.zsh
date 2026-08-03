fe18f4bf2b8efc59af1b70a33ef052152678969e
Seriki Walter Yakub <webapp4@outlook.com>
2026-08-03 23:15:41 +0100

    Update and rename bash.sh to bash.zsh

--- bash.zsh
+++ bash.zsh
@@ -8,3 +8,12 @@ curl https://s3.us.archive.org -v -H "x-archive-simulate-error:SlowDown"
 # List all supported errors for testing
 curl https://s3.us.archive.org -v -H "x-archive-simulate-error:help"
 curl --location "http://archive.org/download/$bucket/$key"
+
+cd simulations
+
+# Attack simulations
+python attack_simulations.py               # Core attack simulation framework
+python attack_track_fb.py                  # Trust manipulation attacks
+python attack_track_fc.py                  # Economic attacks
+
+# For full 4-Life game demos, see: https://github.com/dp-web4/4-life
